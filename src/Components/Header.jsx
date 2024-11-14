ca468e40fc937f8330a8b07c32732fa80a7e5a43
AAYUSHBISHNOI <aayushkaswan79@gmail.com>
2024-11-14 15:33:51 +0530

    Initial commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const Header = () => {
+  return (
+    <div>
+      <Container>
+        <h1>Siddharth </h1>
+      </Container>
+    </div>
+  )
+}
+
+export default Header
